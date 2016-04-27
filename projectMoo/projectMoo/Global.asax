aeee0ead46fa80a7e842b11dd9f7f3637c280211
Gunnar Bjarki <gunnarbb15@ru.is>
2016-04-27 13:32:26 +0000

    initial project

---
+++ projectMoo/projectMoo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="projectMoo.MvcApplication" Language="C#" %>
---
+++ projectMoo/projectMoo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(projectMoo.Startup))]
+namespace projectMoo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
