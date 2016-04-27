93dcb9ee63cceafe193a012aa3db5f827f843da1
Hjalti Steinar <hjalti15@ru.is>
2016-04-27 14:33:06 +0000

    fine tune

--- projectMoo/projectMoo/Controllers/HomeController.cs
+++ projectMoo/projectMoo/Controllers/HomeController.cs
@@ -8,7 +8,6 @@ namespace projectMoo.Controllers
 {
     public class HomeController : Controller
     {
-        [Authorize]
         public ActionResult Index()
         {
             return View();
